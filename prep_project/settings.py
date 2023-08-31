9eee314f75cce366a68f119172ccd075190ef9c7
williamsaa <68300474+williamsaa@users.noreply.github.com>
2023-08-31 14:37:13 -0500

    Update settings.py

--- prep_project/settings.py
+++ prep_project/settings.py
@@ -74,7 +74,7 @@ ROOT_URLCONF = 'prep_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
