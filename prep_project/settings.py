e6bdefb4e710c2777ccaa15fd2fc4b5a6c9850b8
williamsaa <68300474+williamsaa@users.noreply.github.com>
2023-08-31 14:36:50 -0500

    Update settings.py

--- prep_project/settings.py
+++ prep_project/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+TEMPLATE_DIR = '/home/williamsaa/prep_project/accounts/templates/'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
