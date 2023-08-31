67ebbc38cec6e479c24a97f851ad231fb2a08237
williamsaa <68300474+williamsaa@users.noreply.github.com>
2023-08-31 16:39:36 -0500

    Update settings.py

--- prep_project/settings.py
+++ prep_project/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-TEMPLATE_DIR = '/home/williamsaa/prep_project/accounts/templates/'
+TEMPLATE_DIR = '/home/williamsaa/prep_project/accounts/'
 
 
 # Quick-start development settings - unsuitable for production
