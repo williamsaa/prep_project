d40a9fce42d0a568673afd3329fa873b56b75402
williamsaa <68300474+williamsaa@users.noreply.github.com>
2023-09-01 01:04:46 -0500

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
+from django.conf.urls import include, url
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login_user', views.login_user, name='login_user'),
-    path('logout', auth_views.LogoutView.as_view(), name='logout'),
-
-    
+    url(r'^login_user/', views.login_user, name='login_user'),
+    url(r'^logout/', auth_views.LogoutView.as_view(), name='logout'),
+ 
 ] 
