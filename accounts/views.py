a59b3f7011392f45960b62ab452e9abfd081d744
williamsaa <68300474+williamsaa@users.noreply.github.com>
2023-09-01 10:55:35 -0500

    Add files via upload

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render,  redirect
+from django.shortcuts import render, redirect
 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from prep_app.models import UserFacilityAssignment
 from django.db.models import Q
 from django.contrib.auth.models import User
+
 
 def login_user(request):
     if request.method == "POST":
@@ -25,15 +26,13 @@ def login_user(request):
                 user_id = user1.id
                 request.session['User_id'] = user_id
 
-               
-
-                #login(request, user)
-                return redirect ('client_list')
+                login(request, user)
+                return redirect('client_list')
             else:
-                messages.success(request,'Username or password or site selected is incorrect')
-                return redirect ('login_user')
+                messages.success(request, 'Username or password or site selected is incorrect')
+                return redirect('login_user')
         else:
-            messages.success(request,'Username or password or site selected is incorrect')
-            return redirect ('login_user')    
-            
-return render(request, 'authentication\login.html')
+            messages.success(request, 'Username or password or site selected is incorrect')
+            return redirect('login_user')
+
+    return render(request, 'authentication/login.html')
