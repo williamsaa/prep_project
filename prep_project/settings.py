c6ea18f540231a93f0caf390b19045ae9d22d69a
williamsaa <68300474+williamsaa@users.noreply.github.com>
2023-09-01 00:48:53 -0500

    Update settings.py

--- prep_project/settings.py
+++ prep_project/settings.py
@@ -9,13 +9,13 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-TEMPLATE_DIR = ('/home/williamsaa/prep_project/accounts/')
+#TEMPLATE_DIR = ('/home/williamsaa/prep_project/accounts/')
 
 
 # Quick-start development settings - unsuitable for production
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'prep_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [os.path.join(BASE_DIR, 'accounts', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
