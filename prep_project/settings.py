db64f8daed9ea0207166432f43aba3b761fe76fe
williamsaa <68300474+williamsaa@users.noreply.github.com>
2023-09-01 02:40:34 -0500

    Add files via upload

--- prep_project/settings.py
+++ prep_project/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'prep_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'accounts', 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'prep_project', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
