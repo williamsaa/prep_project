b98a03977eabf2637a2925c4bfd8a56a26d0d75a
williamsaa <68300474+williamsaa@users.noreply.github.com>
2023-11-24 11:38:35 -0500

    Update settings.py

--- prep_project/settings.py
+++ prep_project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=8%go6c5d8h*)pvkqezv+sg@l*n3o+9=dx0xo9%sf(fxw$4kk_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 #ALLOWED_HOSTS = ['*']
 #ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'williamsaa.pythonanywhere.com']
