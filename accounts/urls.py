044019213e3139c79fe2afa242fe19f0c898584b
williamsaa <68300474+williamsaa@users.noreply.github.com>
2024-03-03 13:37:13 -0500

    Add files via upload

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('login_user', views.login_user, name='login_user'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
- 
+
+    
 ] 
