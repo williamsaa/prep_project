75370a3e0554474febe7ec2f33539a4c6da039dd
williamsaa <68300474+williamsaa@users.noreply.github.com>
2024-03-06 12:00:58 -0500

    Delete prep_app directory

--- prep_app/autocomplete.py
+++
@@ -1,7 +0,0 @@
-# your_app/autocomplete.py
-
-import autocomplete_light
-
-from .models import Community  # Replace with the appropriate model
-
-autocomplete_light.register(Community)  # Replace with the appropriate model
